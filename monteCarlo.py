2724eb786dd1009e8b2609e2a848d5c904f9a779
mizan <mizan@gmail.com>
2024-07-25 17:52:18 +0700

    update monteCarlo.py

--- monteCarlo.py
+++ monteCarlo.py
@@ -35,7 +35,7 @@ with st.sidebar :
                                    "menu-title" : {"font-family" : 'Poppins', "font-weight" : "700"},
                                    "nav-link-selected" : {"font-weight" : "700", "background-color" : "#dc3545"},
                                    "icon" : {"font-size" : "30px"},
-                                   "nav-link" : {"--hover-color" : "#dc3545", "display":"flex", "align-items":"center", "gap": "5px"}})
+                                   "nav-link" : {"--hover-color" : "#dc3545", "display":"flex", "align-items":"center", "gap": "1.5rem"}})
     
 if (selected == '1. Deskripsi Model') :
     st.header(f"Penjelasan Mengenai Model yang akan dibuat")
